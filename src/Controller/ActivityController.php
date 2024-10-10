c842207dbc3b31e174f7f93fd3d857b3e9b712a8
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-10-10 17:01:32 +0200

    Activity Controller

---
+++ src/Controller/ActivityController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Activity;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/api/activity', name: 'app_activity_')]
+class ActivityController extends AbstractController
+{
+    #[Route('/create', name: 'create', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function create(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse
+    {
+        $activity = $serializer->deserialize($request->getContent(), Activity::class, 'json');
+
+        $errors = $validator->validate($activity);
+
+        if (count($errors) > 0) {
+            $errorsArray = [];
+            foreach ($errors as $error) {
+                $errorsArray[] = $error->getMessage();
+            }
+            return new JsonResponse(['errors' => $errorsArray], Response::HTTP_BAD_REQUEST);
+        }
+
+        $entityManager->persist($activity);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'Activity created'], Response::HTTP_CREATED);
+    }
+
+    #[Route('/list', name: 'list', methods: ['GET'])]
+    public function list(EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+
+        $activities = $entityManager->getRepository(Activity::class)->findAll();
+
+        $data = $serializer->normalize($activities, null, ['groups' => ['read']]);
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+}
--- src/Entity/Activity.php
+++ src/Entity/Activity.php
@@ -7,26 +7,47 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ActivityRepository::class)]
+#[UniqueEntity(fields: ['name'], message: 'This activity already exists')]
 class Activity
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Activity name cannot be empty')]
+    #[Assert\Length(min: 3, max: 255, minMessage: 'Activity name must be at least 3 characters long', maxMessage: 'Activity name cannot be longer than 255 characters')]
+    #[Groups(['read'])]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: 'Activity description cannot be empty')]
+    #[Assert\Length(min: 10, max: 500, minMessage: 'Activity description must be at least 10 characters long', maxMessage: 'Activity description cannot be longer than 255 characters')]
+    #[Groups(['read'])]
     private ?string $description = null;
 
     /**
      * @var Collection<int, User>
      */
     #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'activities')]
-    private Collection $participants;
+    private Collection $participants; // Sin grupo para ocultarlo en serialización
+
+    #[ORM\Column]
+    #[Assert\NotBlank(message: 'Activity duration cannot be empty')]
+    #[Assert\Range(
+        min: 30,
+        max: 120,
+        notInRangeMessage: 'Activity duration must be between {{ min }} and {{ max }} minutes'
+    )]
+    #[Groups(['read'])]
+    private ?int $duration = null;
 
     public function __construct()
     {
@@ -82,6 +103,18 @@ class Activity
     public function removeParticipant(User $participant): static
     {
         $this->participants->removeElement($participant);
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): static
+    {
+        $this->duration = $duration;
 
         return $this;
     }
